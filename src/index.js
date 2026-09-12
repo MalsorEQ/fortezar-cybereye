826783bf6e9529a53b7c367079413308f419ed13
MalsorEQ <MalsorEQ22@protonmail.com>
2026-09-12 23:07:17 +0200

    feat: add public module exports

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { scanTarget, safeFetch } from './scanner.js';
+export { evaluateRules } from './rules/index.js';
+export { toSarif } from './reporters/sarif.js';
+export { toJson } from './reporters/json.js';
